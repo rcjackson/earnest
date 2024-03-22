9cae3091ed5985d9724432358856e6f9134c2a1b
Robert Jackson <rjackson@anl.gov>
2024-03-22 10:24:08 -0500

    newest changes

--- scripts/dealias_data.py
+++ scripts/dealias_data.py
@@ -16,7 +16,7 @@ radar = sys.argv[1]
 out_plot_path = os.path.join(
     '/lcrc/group/earthscience/rjackson/Earnest/quicklooks_dealias/', radar)
 rad_path = os.path.join(
-    '/lcrc/group/earthscience/rjackson/Earnest/', radar)
+    '/lcrc/group/earthscience/rjackson/Earnest/pre_processed/', radar)
 out_proc_path = os.path.join(
     '/lcrc/group/earthscience/rjackson/Earnest/processed/', radar)
 
@@ -26,53 +26,12 @@ if not os.path.exists(out_proc_path):
 if not os.path.exists(out_plot_path):
     os.makedirs(out_plot_path)
 
-def make_quicklooks(file):    
+def make_quicklooks(file, prev_file):    
     try:
         rad = pyart.io.read(file)
     except TypeError:
         return
     base, name = os.path.split(file)
-    # Let's actually have a coherent NEXRAD scan
-    which_sweeps_vel = []
-    which_elevations = []
-    for i, s in enumerate(rad.iter_slice()):
-        sum_vel = np.ma.sum(rad.fields['velocity']['data'][s, :])
-        elevation = rad.fixed_angle['data'][i]
-        if not np.ma.is_masked(sum_vel) and not elevation in which_elevations:
-            which_sweeps_vel.append(i)
-            which_elevations.append(elevation)
-        
-    fields_needed = ['differential_phase',
-            'cross_correlation_ratio',
-            'reflectivity',
-            'differential_reflectivity']
-
-    rad_dest = rad.extract_sweeps(which_sweeps_vel)
-    sum_ref = np.ma.masked
-    for i, sweep in enumerate(which_sweeps_vel):
-        for cand_sweep in np.argwhere(
-            np.isclose(rad.fixed_angle['data'], rad.fixed_angle['data'][sweep])):
-            sl = rad.get_slice(cand_sweep)
-            sum_ref = np.ma.sum(
-                rad.fields['cross_correlation_ratio']['data'][
-                    int(sl.start):int(sl.stop), :])
-            src_slice = rad.get_slice(sweep)
-            if not np.ma.is_masked(sum_ref) and (sl.stop - sl.start) == (src_slice.stop - src_slice.start):
-                break
-        dest_slice = rad_dest.get_slice(i)
-        dest_azi = rad_dest.azimuth['data'][dest_slice.start:dest_slice.stop]
-        src_azi = rad.azimuth['data'][int(sl.start):int(sl.stop)]
-        tree = KDTree(dest_azi[:, np.newaxis])
-        neighbors, indices = tree.query(
-            src_azi[:, np.newaxis], distance_upper_bound=1)
-        for field in fields_needed:
-            offset = min([dest_slice.start, dest_slice.stop])
-            rad_dest.fields[field]['data'][indices + offset, :] = rad.fields[field]['data'][int(sl.start):int(sl.stop), :]
-    rad_gatefilter = pyart.filters.GateFilter(rad_dest)
-    del rad
-    text_phase = pyart.bridge.texture_of_complex_phase(rad_dest)
-    rad_dest.add_field('texture_of_differential_phase',
-        text_phase, replace_existing=True)
     disp = pyart.graph.RadarMapDisplay(rad_dest)
     gatefilter = pyart.filters.GateFilter(rad_dest)
     gatefilter.exclude_above('texture_of_differential_phase', 50)
@@ -81,7 +40,23 @@ def make_quicklooks(file):
         rad_dest, 'velocity', gatefilter=gatefilter)
     corrected_velocity = pyart.correct.dealias_region_based(
         rad_dest, gatefilter=gatefilter, centered=True, skip_between_rays=100, skip_along_rays=1000)
+    if prev_file is None:
+        use_previous = False
+    else:
+        rad_prev = pyart.io.read(prev_file)
+        if prev_file.fields['velocity'].shape == file.fields['velocity'].shape:
+            rad_dest.fields['previous_velocity'] = file.fields['corrected_velocity_region_based']
+            use_previous = True
+        else:
+            use_previous= False
+    if use_previous is False:
+        gatefilter = pyart.correct.despeckle_field(
+            rad_dest, 'velocity', gatefilter=gatefilter)
+    else:
+        gatefilter = pyart.correct.despeckle_field(
+            rad_dest, 'velocity', gatefilter=gatefilter, ref_vel_field='previous_velocity')
     rad_dest.add_field('corrected_velocity_region_based', corrected_velocity, replace_existing=True)
+    
     dealiased_velocity = unravel.unravel_3D_pyart(
         rad_dest, velname='velocity', dbzname='reflectivity', do_3d=True)
     rad_dest.add_field_like('velocity', 'corrected_velocity_unravel',
@@ -120,9 +95,8 @@ def make_quicklooks(file):
 
 if __name__ == "__main__":
     file_list = sorted(glob.glob(rad_path + '/*'))
-    cluster = SLURMCluster(processes=6, cores=36, memory='128GB', walltime='6:00:00') 
-    cluster.scale(24)
-    with Client(cluster) as c:
-        c.wait_for_workers(6)
-        results = c.map(make_quicklooks, file_list)
-        wait(results)
+    for i in range(len(file_list)):
+        if i > 1:
+            make_quicklooks(file_list[i], file_list[i - 1])
+        else:
+            make_quicklooks(file_list[i], None)
---
+++ scripts/preprocess_data.py
@@ -0,0 +1,84 @@
+import pyart
+import cmweather
+import glob
+import matplotlib.pyplot as plt
+import sys
+import os
+import numpy as np
+import cartopy.crs as ccrs
+import unravel
+
+from distributed import Client, wait
+from dask_jobqueue import SLURMCluster
+from scipy.spatial import KDTree
+
+radar = sys.argv[1]
+rad_path = os.path.join(
+    '/lcrc/group/earthscience/rjackson/Earnest/', radar)
+out_proc_path = os.path.join(
+    '/lcrc/group/earthscience/rjackson/Earnest/pre_processed/', radar)
+
+if not os.path.exists(out_proc_path):
+    os.makedirs(out_proc_path)
+
+
+def make_quicklooks(file):    
+    try:
+        rad = pyart.io.read(file)
+    except TypeError:
+        return
+    base, name = os.path.split(file)
+    # Let's actually have a coherent NEXRAD scan
+    which_sweeps_vel = []
+    which_elevations = []
+    for i, s in enumerate(rad.iter_slice()):
+        sum_vel = np.ma.sum(rad.fields['velocity']['data'][s, :])
+        elevation = rad.fixed_angle['data'][i]
+        if not np.ma.is_masked(sum_vel) and not elevation in which_elevations:
+            which_sweeps_vel.append(i)
+            which_elevations.append(elevation)
+        
+    fields_needed = ['differential_phase',
+            'cross_correlation_ratio',
+            'reflectivity',
+            'differential_reflectivity']
+
+    rad_dest = rad.extract_sweeps(which_sweeps_vel)
+    sum_ref = np.ma.masked
+    for i, sweep in enumerate(which_sweeps_vel):
+        for cand_sweep in np.argwhere(
+            np.isclose(rad.fixed_angle['data'], rad.fixed_angle['data'][sweep])):
+            sl = rad.get_slice(cand_sweep)
+            sum_ref = np.ma.sum(
+                rad.fields['cross_correlation_ratio']['data'][
+                    int(sl.start):int(sl.stop), :])
+            src_slice = rad.get_slice(sweep)
+            if not np.ma.is_masked(sum_ref) and (sl.stop - sl.start) == (src_slice.stop - src_slice.start):
+                break
+        dest_slice = rad_dest.get_slice(i)
+        dest_azi = rad_dest.azimuth['data'][dest_slice.start:dest_slice.stop]
+        src_azi = rad.azimuth['data'][int(sl.start):int(sl.stop)]
+        tree = KDTree(dest_azi[:, np.newaxis])
+        neighbors, indices = tree.query(
+            src_azi[:, np.newaxis], distance_upper_bound=1)
+        for field in fields_needed:
+            offset = min([dest_slice.start, dest_slice.stop])
+            rad_dest.fields[field]['data'][indices + offset, :] = rad.fields[field]['data'][int(sl.start):int(sl.stop), :]
+    rad_gatefilter = pyart.filters.GateFilter(rad_dest)
+    del rad
+    text_phase = pyart.bridge.texture_of_complex_phase(rad_dest)
+    rad_dest.add_field('texture_of_differential_phase',
+        text_phase, replace_existing=True)
+
+    pyart.io.write_cfradial(os.path.join(out_proc_path, name + '.nc'), rad_dest)
+    del fig
+    print("Preprocessing for %s completed!" % file)
+
+if __name__ == "__main__":
+    file_list = sorted(glob.glob(rad_path + '/*'))
+    cluster = SLURMCluster(processes=6, cores=36, memory='128GB', walltime='6:00:00') 
+    cluster.scale(24)
+    with Client(cluster) as c:
+        c.wait_for_workers(6)
+        results = c.map(make_quicklooks, file_list)
+        wait(results)
